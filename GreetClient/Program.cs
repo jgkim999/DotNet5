3944aba3ec0e678cd3f622b6aad61c8e445d1ab2
jgkim <jgkim999@gmail.com>
2020-11-18 23:37:10 +0900

    create greetclient

---
+++ GreetClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreetClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
